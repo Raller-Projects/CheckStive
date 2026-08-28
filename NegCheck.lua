8386eb152c4979be6f90772b9f3f012d6df93c67
Emralix.exe <mshahkhairpur@outlook.com>
2026-08-28 22:00:03 +0500

    Add Intial first file

---
+++ NegCheck.lua
@@ -0,0 +1,43 @@
+--Number checker
+print("===Welcome to Negstive number checker===")
+while true do
+  
+  io.write("Enter Number>>")
+  io.flush()
+
+  local query = tonumber(io.read())
+
+  if query == nil then
+    print("Oh!, ALphabets and invalid inputs are not processed here. Please try again with a number!!")
+  end
+
+  if query ~= nil and query <= 0 then
+   print("Result:")
+    print("Negative Number")
+  end
+
+  if query ~= nil and query >= 0 then
+    print("Result:")
+    print("Positive Number")
+ end
+
+  if query ~= nil and query == 0 then
+    print("Result:")
+    print("0 has no value and is Neutral")
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
