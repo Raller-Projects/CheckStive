9c8a798bf54c0d99642d5e3c52ea92709253e1a1
Mathdallas(Jaiwant) <mathdallasrobolx@gmail.com>
2026-08-29 21:45:43 +0530

    Fix bugs

--- NegStive.lua
+++ NegStive.lua
@@ -2,34 +2,19 @@
 print("=== Welcome to NegStive ===")
 while true do
   
-  io.write("Enter Number >")
+  io.write("Enter Number > ")
   io.flush()
 
   local query = tonumber(io.read())
 
   if query == nil then
     print("Oh!, Alphabets and invalid inputs are not processed here. Please try again with a number!")
+  elseif query < 0 then
+    print("Negative Number")
+  elseif query > 0 then
+    print("Positive Number")
   else
-    if query < 0 then
-      print("Negative Number")
-    else if query > 0 then
-      print("Positive Number")
-    else
-      print("The number is 0, neither positive nor negative")
+    print("The number is 0, neither positive nor negative")
   end
+
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
